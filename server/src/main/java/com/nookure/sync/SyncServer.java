f78aee76348af31a307e51602f2524126ebc42e5
Angelillo15 <contact@angelillo15.es>
2024-12-30 13:52:42 +0100

    feat: add Zlib compression and decompression

--- server/src/main/java/com/nookure/sync/SyncServer.java
+++ server/src/main/java/com/nookure/sync/SyncServer.java
@@ -5,8 +5,10 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.codec.compression.JZlibEncoder;
+import io.netty.handler.codec.compression.JdkZlibDecoder;
+import io.netty.handler.codec.compression.ZlibWrapper;
+import org.tinylog.Logger;
 
 public class SyncServer {
   public void start() throws InterruptedException {
@@ -20,12 +22,13 @@ public class SyncServer {
           .childHandler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel channel) throws Exception {
-              channel.pipeline().addLast(new StringDecoder());
-              channel.pipeline().addLast(new StringEncoder());
+              channel.pipeline().addLast("compressor", new JdkZlibDecoder(ZlibWrapper.GZIP));
+              channel.pipeline().addLast("decompressor", new JZlibEncoder(ZlibWrapper.GZIP));
             }
           });
 
       ChannelFuture future = bootstrap.bind(8080).sync();
+      Logger.info("Server started and listening on port 8080");
       future.channel().closeFuture().sync();
     } finally {
       bossGroup.shutdownGracefully();

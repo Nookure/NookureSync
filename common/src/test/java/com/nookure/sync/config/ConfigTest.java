8197dbb7950ebae00cecd9bac21ea617304f9d28
Angelillo15 <contact@angelillo15.es>
2025-01-04 00:37:48 +0100

    config: create ConfigurationContainer and make a test

---
+++ common/src/test/java/com/nookure/sync/config/ConfigTest.java
@@ -0,0 +1,27 @@
+package com.nookure.sync.config;
+
+import de.exlll.configlib.Comment;
+import de.exlll.configlib.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+public class ConfigTest {
+  @Configuration
+  public static class Config {
+    @Comment("This is a key")
+    public String key = "value";
+  }
+
+  @Test
+  public void basicTest() {
+    var path = Path.of("build", "test", "config");
+
+    var container = ConfigurationContainer.load(path, "config.yml", Config.class);
+    var config = container.get();
+    config.key = "new value";
+    container.save().join();
+
+    assert container.get().key.equals("new value");
+  }
+}

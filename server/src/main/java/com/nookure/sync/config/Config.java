02a5f500d5bc402c479b853d142345fb03bcc126
Angelillo15 <contact@angelillo15.es>
2025-01-04 00:39:56 +0100

    config(server): create

---
+++ server/src/main/java/com/nookure/sync/config/Config.java
@@ -0,0 +1,17 @@
+package com.nookure.sync.config;
+
+import de.exlll.configlib.Comment;
+import de.exlll.configlib.Configuration;
+
+@Configuration
+public class Config {
+  @Comment("Here you can configure the netty server configuration.")
+  public ServerConfig server = new ServerConfig();
+
+  @Override
+  public String toString() {
+    return "Config{" +
+        "server=" + server +
+        '}';
+  }
+}
---
+++ server/src/main/java/com/nookure/sync/config/ServerConfig.java
@@ -0,0 +1,23 @@
+package com.nookure.sync.config;
+
+import de.exlll.configlib.Comment;
+import de.exlll.configlib.Configuration;
+
+@Configuration
+public class ServerConfig {
+  @Comment("The port the server should listen on.")
+  public int port = 8080;
+  @Comment("The host the server should listen on.")
+  public String host = "localhost";
+  @Comment("Whether the server should use compression.")
+  public boolean compression = true;
+
+  @Override
+  public String toString() {
+    return "ServerConfig{" +
+        "port=" + port +
+        ", host='" + host + '\'' +
+        ", compression=" + compression +
+        '}';
+  }
+}

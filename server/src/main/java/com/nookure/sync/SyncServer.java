d1ffd4ebd73709b431267b72509f1da86b50d918
Angelillo15 <contact@angelillo15.es>
2025-01-04 00:40:25 +0100

    feat: inject Config

--- server/src/main/java/com/nookure/sync/SyncServer.java
+++ server/src/main/java/com/nookure/sync/SyncServer.java
@@ -1,5 +1,8 @@
 package com.nookure.sync;
 
+import com.google.inject.Inject;
+import com.nookure.sync.config.Config;
+import com.nookure.sync.config.ConfigurationContainer;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -8,9 +11,21 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.compression.JZlibEncoder;
 import io.netty.handler.codec.compression.JdkZlibDecoder;
 import io.netty.handler.codec.compression.ZlibWrapper;
-import org.tinylog.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
 
 public class SyncServer {
+  private final ConfigurationContainer<Config> config;
+  private final Logger logger = LoggerFactory.getLogger("NookureSync");
+
+  @Inject
+  public SyncServer(@NotNull final ConfigurationContainer<Config> config) {
+    this.config = config;
+  }
+
   public void start() throws InterruptedException {
     EventLoopGroup bossGroup = new NioEventLoopGroup();
     EventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -22,13 +37,16 @@ public class SyncServer {
           .childHandler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel channel) throws Exception {
-              channel.pipeline().addLast("compressor", new JdkZlibDecoder(ZlibWrapper.GZIP));
-              channel.pipeline().addLast("decompressor", new JZlibEncoder(ZlibWrapper.GZIP));
+              if (config.get().server.compression) {
+                channel.pipeline().addLast("compressor", new JdkZlibDecoder(ZlibWrapper.GZIP));
+                channel.pipeline().addLast("decompressor", new JZlibEncoder(ZlibWrapper.GZIP));
+              }
             }
           });
 
-      ChannelFuture future = bootstrap.bind(8080).sync();
-      Logger.info("Server started and listening on port 8080");
+      ChannelFuture future = bootstrap.bind(new InetSocketAddress(config.get().server.host, config.get().server.port)).sync();
+      logger.info("Server started on {}:{}", config.get().server.host, config.get().server.port);
+      if (config.get().server.compression) logger.info("Using zlib compression.");
       future.channel().closeFuture().sync();
     } finally {
       bossGroup.shutdownGracefully();
